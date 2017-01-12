27d9910af9ed1d5aac4ee3f32d3466a9f6bde71c
Matt Riemer <matthew@agilestyle.com>
2017-01-12 10:08:47 -0700

    Restore module

---
+++ lib/effective_developer.rb
@@ -0,0 +1,18 @@
+require 'generators/effective/helpers'
+require 'effective_developer/engine'
+require 'effective_developer/version'
+
+module EffectiveDeveloper
+
+  def self.setup
+    yield self
+  end
+
+  def self.authorized?(controller, action, resource)
+    if authorization_method.respond_to?(:call) || authorization_method.kind_of?(Symbol)
+      raise Effective::AccessDenied.new() unless (controller || self).instance_exec(controller, action, resource, &authorization_method)
+    end
+    true
+  end
+
+end
