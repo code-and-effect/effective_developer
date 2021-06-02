ca1a7862e3631beee776d04137262e6846d9daf4
Matt Riemer <matthew@codeandeffect.com>
2021-06-02 14:50:18 -0600

    form upgrader wip

--- app/models/effective/form_upgrader.rb
+++ app/models/effective/form_upgrader.rb
@@ -14,12 +14,15 @@ module Effective
 
           writer = Effective::CodeWriter.new(path)
 
+          name = path.split('/')[0...-1] - ['app', 'views']
+          resource = Effective::Resource.new(name)
+
           if writer.find { |line| line.include?('simple_form_for') }
-            upgrade_simple_form(writer)
+            upgrade_simple_form(writer, resource)
           elsif writer.find { |line| line.include?('semantic_form_for') }
-            upgrade_formtastic(writer)
+            upgrade_formtastic(writer, resource)
           elsif writer.find { |line| line.include?('form_for') }
-            upgrade_form_for(writer)
+            upgrade_form_for(writer, resource)
           else
             next # Nothign to do
           end
@@ -34,31 +37,47 @@ module Effective
 
     private
 
-    # simple_form_for [:admin, resource]
-    # simple_form_for resource
     SIMPLE_FORM_FOR_REGEX = /simple_form_for( |\()(\[:\w+, ?\w+\])?((\w+),)?.+do \|(\w+)\|/
+    SIMPLE_FORM_INPUT_ATTRIBUTE = /\.input( |\():(\w+),?/
+    SIMPLE_FORM_INPUT_AS_ONE = /(as: :(\w+))/
+    SIMPLE_FORM_INPUT_AS_TWO = /(:as => :(\w+))/
 
-    def upgrade_simple_form(writer)
+    def upgrade_simple_form(writer, resource)
       puts "Upgrading simple form: #{writer.filename}"
 
-      # Replace simple_form_for lines
+      # Replace simple_form_for
       writer.all { |line| line.include?('simple_form_for') }.each do |line|
         content = writer.lines[line]
         matched = content.match(SIMPLE_FORM_FOR_REGEX)
+        raise("unable to match simple_form_for from:\n#{content}") unless matched.present?
 
         original = matched[0]
         model = matched[2] || matched[4]
         letter = matched[5]
 
-        raise("unable to determine simple_form_for subject") unless original && model && letter
+        raise("unable to determine simple_form_for subject from:\n#{content}") unless original && model && letter
 
-        content.gsub!(original, "effective_form_with(model: #{model}) do |#{letter}|")
+        content.sub!(original, "effective_form_with(model: #{model}) do |#{letter}|")
         writer.replace(line, content)
       end
 
       # Replace .input
       writer.all { |line| line.include?('.input :') }.each do |line|
-        content = writer.lines[line].gsub('.input', '.input_was')
+        content = writer.lines[line]
+        attribute = content.match(SIMPLE_FORM_INPUT_ATTRIBUTE)
+        raise("unable to match simple_form_for input attribute from\n#{content}") unless attribute.present?
+
+        as = content.match(SIMPLE_FORM_INPUT_AS_ONE) || content.match(SIMPLE_FORM_INPUT_AS_TWO)
+
+        if as.present?
+          content.sub!("#{as[0]}, ", '')
+          content.sub!("#{as[0]},", '')
+          content.sub!(as[0], '')
+        end
+
+        input_type = find_input_type(resource: resource, attribute: attribute[2], as: (as[2] if as))
+
+        content.sub!('input', input_type)
         writer.replace(line, content)
       end
 
@@ -70,26 +89,28 @@ module Effective
 
     end
 
-    def upgrade_formtastic(writer)
+    def upgrade_formtastic(writer, resource)
       puts "Detected formtastic: #{writer.filename}"
     end
 
-    def upgrade_form_for(writer)
+    def upgrade_form_for(writer, resource)
       puts "Detected rails form_for: #{writer.filename}"
     end
 
-    # def upgrade(path)
-    #   puts "Annotate: #{path}"
+    def find_input_type(attribute:, resource:, as: nil)
+      input_type = (as || resource.sql_type(attribute)).to_s
 
-    #   Effective::CodeWriter.new(path) do |writer|
-    #     index = find_insert_at(writer)
-    #     content = build_content(resource)
-
-    #     remove_existing(writer)
-    #     writer.insert(content, index)
-    #   end
-    # end
-
+      case input_type
+      when 'effective_date_picker' then 'date_field'
+      when 'select', 'effective_select' then 'select'
+      when 'boolean' then 'check_box'
+      when 'text' then 'text_area'
+      when 'string' then 'text_field'
+      when 'integer' then 'number_field'
+      else
+        raise("unknown input type for #{attribute} of type #{input_type}")
+      end
+    end
 
   end
 end
