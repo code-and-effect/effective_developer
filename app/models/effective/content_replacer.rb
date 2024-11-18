3b25e7cbe103873dbce9bf8bcb1f04660aeeecb6
Matt Riemer <matthew@codeandeffect.com>
2024-11-18 09:27:25 -0700

    Add find alias for content replacer

--- app/models/effective/content_replacer.rb
+++ app/models/effective/content_replacer.rb
@@ -16,11 +16,14 @@ module Effective
     def replace!(old_value, new_value)
       raise("old_value cannot contain a \' character") if old_value.include?("'")
       raise("new_value cannot contain a \' character") if new_value.include?("'")
+
+      total = 0
   
       places.each do |table, columns|
         columns.each do |column|
           sql = "SELECT COUNT(*) FROM #{table} WHERE #{column} ILIKE '%#{old_value}%'"
           existing = ActiveRecord::Base.connection.execute(sql).first['count'].to_i
+          total += existing
 
           puts "Replacing #{existing} occurrences of #{old_value} with #{new_value} in #{table}.#{column}"
 
@@ -28,22 +31,28 @@ module Effective
           ActiveRecord::Base.connection.execute(sql)
         end
       end
-      
-      true
+
+      total
     end
 
     def count(old_value)
       raise("old_value cannot contain a \' character") if old_value.include?("'")
 
+      total = 0
+
       places.each do |table, columns|
         columns.each do |column|
           sql = "SELECT COUNT(*) FROM #{table} WHERE #{column} ILIKE '%#{old_value}%'"
           existing = ActiveRecord::Base.connection.execute(sql).first['count'].to_i
+          total += existing
 
           puts "There are #{existing} occurrences of #{old_value} in #{table}.#{column}"
         end
       end
+
+     total 
     end
+    alias_method :find, :count
 
   end
 end
